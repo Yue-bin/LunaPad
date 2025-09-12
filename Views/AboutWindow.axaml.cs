e68598defd1a64f765b3bdb6a6cd0e4911165eed
Yue-bin <3103174284@qq.com>
2025-09-13 00:46:10 +0800

    ✨ feat: new about window

---
+++ Views/AboutWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LunaPad.Views;
+
+public partial class AboutWindow : UserControl
+{
+    public AboutWindow()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
